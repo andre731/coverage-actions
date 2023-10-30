f4f27975649b466aa0240986cc909d6955d9baec
Andre Junior <andre.junior@grupo-eagle.com>
2023-10-30 17:58:03 -0300

    test pr 2

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  console.log("RAIZZZZZZZZZZZZZZZZZZZZZ " + PORT);
 
 
   return res.json({ message: "Olá, mundo!" });
+});
+
+app.get("/2", async (req, res) => {
+  return res.json({ message: "ROTA 2" });
 });
 
 const server = app.listen(PORT, () => {
