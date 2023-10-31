5cc2c952cecce7b1694cfd5461130aa6afae6061
Andre Junior <andre.junior@grupo-eagle.com>
2023-10-31 09:17:18 -0300

    fix test

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -5,7 +5,7 @@ describe('Testando a API', () => {
   it('Deve retornar a mensagem "Olá, mundo!" na raiz', async () => {
     const response = await request(api.app).get('/');
     expect(response.status).toBe(200);
-    expect(response.body.message).toBe('ROTA INICIAL');
+    expect(response.body.message).toBe('PATATI');
   });
 });
 
